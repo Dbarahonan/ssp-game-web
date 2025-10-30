8cc6f94309f8cc0193208ca5990f371abd64a787
Diego Barahona <diegoba29@gmail.com>
2025-10-30 18:20:28 +0100

    feat(ssp): add basic logic for ssp component

--- src/app/components/stone-scissors-paper/stone-scissors-paper.ts
+++ src/app/components/stone-scissors-paper/stone-scissors-paper.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stone-scissors-paper',
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './stone-scissors-paper.html',
   styleUrl: './stone-scissors-paper.scss',
   standalone: true
 })
 export class StoneScissorsPaper {
+  options = ['Stone', 'Paper', 'Scissors'];
+  playerChoice: string | null = null;
+  computerChoice: string | null = null;
+  result: string | null = null;
+  loading = false;
+
+  constructor() {}
+
+  play(choice: string) {
+    this.loading = true;
+  }
+
+  reset() {
+    this.playerChoice = null;
+    this.computerChoice = null;
+    this.result = null;
+  }
 
 }
