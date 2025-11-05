15560efb81b0157af8ca19df5cf875def82a4e7d
Diego Barahona <diegoba29@gmail.com>
2025-11-05 21:47:13 +0100

    chore(imports): remove unused imports

--- src/app/components/stone-scissors-paper/move-options/move-options.ts
+++ src/app/components/stone-scissors-paper/move-options/move-options.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
